7cd7686c7a5827a5bfbc4a9f8c5e33fe338cfc60
Krish Agarwal <agarwalkrish29@gmail.com>
2025-02-01 02:44:35 +0530

    .

--- app/api/user/get/route.ts
+++ app/api/user/get/route.ts
@@ -4,7 +4,9 @@ import prisma from "../../../lib/prisma";
 export async function GET() {
   try {
     const users = await prisma.user.findMany();
-    return NextResponse.json(users);
+    return NextResponse.json(users,{
+        headers: { 'Access-Control-Allow-Origin': '*' }
+      });
   } catch (error) {
     console.error("Error fetching users:", error);
     return NextResponse.json({error: "Failed to fetch users"});
